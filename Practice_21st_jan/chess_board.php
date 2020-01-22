46a6b3f1db3e36fe5285e92ef79ee1b30c6d5110
Rohit Kotadiya <kotadiya1998@gmail.com>
2020-01-22 14:00:38 +0530

    all Data upto Now

--- Practice_21st_jan/chess_board.php
+++ Practice_21st_jan/chess_board.php
@@ -1,15 +1,17 @@
 <?php
     $number = 8;
+    echo "<table border='1'>";
     for($row = 1; $row <= $number; $row ++) {
+        echo "<tr>";
         for($col = 1; $col <= $number; $col ++) {
             $sum = $row+$col;
             if($sum % 2 != 0 ){
-                echo "<spam style='background-color:black;'>&nbsp;&nbsp;&nbsp;</span>";
+                echo "<td style='background-color:black;'>&nbsp;&nbsp;&nbsp;</td>";
             }else {
-                echo "<spam style='background-color:white;'>&nbsp;&nbsp;&nbsp;</span>";
+                echo "<td style='background-color:white;'>&nbsp;&nbsp;&nbsp;</td>";
             }
         }
-        echo "<br>";
+        echo "</tr>";
     }
-
+    echo "</table>";
 ?>
