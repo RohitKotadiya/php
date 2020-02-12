4d1411fc8e763346df9a8543081dd33050ee2f8d
Rohit Kotadiya <kotadiya1998@gmail.com>
2020-02-12 18:04:31 +0530

    insertion with error handling

--- MVC/App/Controllers/Register.php
+++ MVC/App/Controllers/Register.php
@@ -5,17 +5,22 @@ use \Core\View;
 use \App\Models\User;
 
 class Register extends \Core\BaseController {
+    public $errList = [];
 
     public function registerForm() {
         View::renderTemplate("Register/registrationForm.html");
     }
     public function addUser() {
-        print_r($_POST['register']);
-        $cleanUserData = $this->prepareUserData($_POST['register']);
-        if(User::insertUserData($cleanUserData) ) {
-            echo "<script> alert('Registered Successfully') </script>";
+        $this->validateForm($_POST['register']);
+        if(empty($this->errList)) {
+            $cleanUserData = $this->prepareUserData($_POST['register']);
+            if(User::insertUserData($cleanUserData) ) {
+                echo "<script> alert('Registered Successfully') </script>";
+            }
+        }else {
+            View::renderTemplate("Register/registrationForm.html", ['errList' => $this->errList, 
+                                                                    'postData' => $_POST['register']]);
         }
-
     }
     protected function prepareUserData($data) {
         $preparedData = [];
@@ -30,6 +35,31 @@ class Register extends \Core\BaseController {
             }
         }
         return $preparedData;
+    }
+    protected function validateForm($formData) {
+        $errMsg = "please enter valid ";
+        foreach($formData as $fieldName => $fieldValue) {
+            if(isset($_POST[$fieldName]) && empty($_POST[$fieldName])) {
+                $emptyMsg = "Missing $fieldName";
+                $this->errList[$fieldName] = $emptyMsg;
+            }else {
+                switch($fieldName) {
+                    case 'firstName'    : if(!preg_match('/^[a-zA-Z]*$/', $fieldValue)) {
+                                                $this->errList[$fieldName] = $errMsg . $fieldName;
+                                            }
+                                            break;
+                    case 'emailId'      : if(!filter_var($fieldValue, FILTER_VALIDATE_EMAIL)) {
+                                                $this->errList[$fieldName] = $errMsg . $fieldName;
+                                            }
+                                            break;
+                    case 'confirmPass'  : if($fieldValue !== $_POST['register']['pass']) {
+                                            $this->errList[$fieldName] = $errMsg . $fieldName;
+                                            }
+                                            break;
+                }
+            }
+        }
+        return $this->errList;
     }
 }
 
